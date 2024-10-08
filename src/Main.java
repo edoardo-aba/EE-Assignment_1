69134cf2489808b30af5e6ae3d160acc383fae99
edoardo-aba <edoardoababei2003@gmail.com>
2024-10-08 11:39:09 +0200

    added sorting classes and interface and made example of use of the first one

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Example array to sort
+        Integer[] array = {3, 5, 2, 1, 4};
+
+        // Create an instance of QuickSortGPT
+        QuickSortGPT<Integer> quickSorter = new QuickSortGPT<>();
+
+        // Sort the array
+        quickSorter.sort(array);
+
+        // Print the sorted array
+        System.out.println("Sorted array using QuickSort:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println(); // Just to add a new line after the sorted output
     }
 }
